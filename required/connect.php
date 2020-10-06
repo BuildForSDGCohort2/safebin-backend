231329c980f7aea63384bce2d8bd700a27e8e1dc
Olayode Enoch <olayodeenoch@gmail.com>
2020-10-06 16:33:06 +0100

    Update connect.php

--- required/connect.php
+++ required/connect.php
@@ -1,5 +1,5 @@
 <?php
-$connect=mysqli_connect("localhost", "root", "", "safebin");
+$connect=mysqli_connect("ec2-34-192-122-0.compute-1.amazonaws.com", "jekhvihjinpysr", "f7e0e76751ed35321d32b35763f3d4e0944e7e3a9c64692d49c50842170d164b", "d23m890snar1vf");
 // Check connection
 if (mysqli_connect_errno())
   {
