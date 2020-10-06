3d889b08b79ca9f4064e1591824146103784ff68
Olayode Enoch <olayodeenoch@gmail.com>
2020-10-06 16:33:20 +0100

    Update dbconfig.php

--- required/dbconfig.php
+++ required/dbconfig.php
@@ -1,9 +1,9 @@
 <?php
 
-	$DB_HOST = 'localhost';
-	$DB_USER = 'root';
-	$DB_PASS = '';
-	$DB_NAME = 'safebin';
+	$DB_HOST = 'ec2-34-192-122-0.compute-1.amazonaws.com';
+	$DB_USER = 'jekhvihjinpysr';
+	$DB_PASS = 'f7e0e76751ed35321d32b35763f3d4e0944e7e3a9c64692d49c50842170d164b';
+	$DB_NAME = 'd23m890snar1vf';
 	
 	try{
 		$DB_con = new PDO("mysql:host={$DB_HOST};dbname={$DB_NAME}",$DB_USER,$DB_PASS);
