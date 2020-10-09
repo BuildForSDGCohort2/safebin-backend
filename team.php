7b9a5f93b22e7194b5d463d471e10345a6df8b3b
Olayode Enoch <olayodeenoch@gmail.com>
2020-10-09 14:20:39 +0100

    Update team.php

--- team.php
+++ team.php
@@ -113,6 +113,20 @@
                                 </div>
                             </div>
                         </div>
+                        <div class="col-md-4 grid-margin stretch-card">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="d-sm-flex flex-row flex-wrap text-center text-sm-left align-items-center">
+                                        <img src="images/team/myde.jpeg" class="img-lg rounded" alt="Olumide Orija" />
+                                        <div class="ml-sm-3 ml-md-0 ml-xl-3 mt-2 mt-sm-0 mt-md-2 mt-xl-0">
+                                            <h6 class="mb-0">Olumide Orija</h6>
+                                            <p class="text-muted mb-1">mrolubanty@gmail.com</p>
+                                            <p class="mb-0 text-success font-weight-bold">Tester</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             <!-- main-panel ends -->
