2a6cde5efcafcac0acaf056a8c60b4ab12b64add
Olayode Enoch <olayodeenoch@gmail.com>
2020-10-09 15:13:58 +0100

    Update connect.php

--- required/connect.php
+++ required/connect.php
@@ -1,5 +1,5 @@
 <?php
-$connect=mysqli_connect("ec2-34-192-122-0.compute-1.amazonaws.com", "jekhvihjinpysr", "f7e0e76751ed35321d32b35763f3d4e0944e7e3a9c64692d49c50842170d164b", "d23m890snar1vf");
+$connect=mysqli_connect("ns541.domainhosting.com.ng", "capitosi_safebin", "safebin", "capitosi_safebin");
 // Check connection
 if (mysqli_connect_errno())
   {
